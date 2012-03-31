bc6221688b807308c797b53dd835372334ca712d
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2012-03-31 22:00:37 +0400

    Update Ads views

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,9 @@ module Omniads
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.initialize_on_precompile = false
+
+    config.active_record.whitelist_attributes = true
   end
 end
